a3503d479db54a1a3a5bfc43963e988786963f81
smehlmann <sandra.mehlmann@gmail.com>
2023-11-14 10:21:37 -0500

    Add files via upload

---
+++ src/components/ReportColumns.jsx
@@ -0,0 +1,226 @@
+export default function ReportColumns({ index, item, selectedReport }) {
+
+    switch (selectedReport) {
+        case '1':
+            return (
+                <tr key={index}>
+                    <td>
+                        {index}
+                    </td>
+                    <td>
+                        {item.emass}
+                    </td>
+                    <td>
+                        {item.collection}
+                    </td>
+                    <td>
+                        {item.benchmark}
+                    </td>
+                    <td>
+                        {item.stigVersion}
+                    </td>
+                    <td>
+                        {item.assetNames}
+                    </td>
+                </tr>
+            );
+        case '2':
+            return (
+                <tr key={index}>
+                    <td>
+                        {index}
+                    </td>
+                    <td>
+                        {item.collectionName}
+                    </td>
+                    <td>
+                        {item.sumOfStigs}
+                    </td>
+                    <td>
+                        {item.sumOfChecks}
+                    </td>
+                    <td>
+                        {item.avgAssessed}
+                    </td>
+                    <td>
+                        {item.avgSubmitted}
+                    </td>
+                    <td>
+                        {item.avgAccepted}
+                    </td>
+                    <td>
+                        {item.avgRejected}
+                    </td>
+                    <td>
+                        {item.sumOfCat3}
+                    </td>
+                    <td>
+                        {item.sumOfCat2}
+                    </td>
+                    <td>
+                        {item.sumOfCat1}
+                    </td>
+                </tr>
+            );
+        case '4':
+            return (
+                <tr key={index}>
+                    <td>
+                        {index}
+                    </td>
+                    <td>
+                        {item.collectionName}
+                    </td>
+                    <td>
+                        {item.assetCount}
+                    </td>
+                </tr>
+            );
+        case '5':
+            return (
+                <tr key={index}>
+                    <td>
+                        {index}
+                    </td>
+                    <td>
+                        {item.collectionName}
+                    </td>
+                    <td>
+                        {item.asset}
+                    </td>
+                    <td>
+                        {item.assessed}
+                    </td>
+                    <td>
+                        {item.submitted}
+                    </td>
+                    <td>
+                        {item.accepted}
+                    </td>
+                    <td>
+                        {item.rejected}
+                    </td> <td>
+                        {item.cat3}
+                    </td>
+                    <td>
+                        {item.cat2}
+                    </td>
+                    <td>
+                        {item.cat1}
+                    </td>
+                </tr>
+            );
+        case '7':
+            return (
+                <tr key={index}>
+                    <td>
+                        {index}
+                    </td>
+                    <td>
+                        {item.emassNum}
+                    </td>
+                    <td>
+                        {item.assetCount}
+                    </td>
+                </tr>
+            );
+        case '8':
+            return (
+                <tr key={index}>
+                    <td>
+                        {index}
+                    </td>
+                    <td>
+                        {item.collectionName}
+                    </td>
+                    <td>
+                        {item.asset}
+                    </td>
+                    <td>
+                        {item.primOwner}
+                    </td>
+                    <td>
+                        {item.sysAdmin}
+                    </td>
+                    <td>
+                        {item.benchmarks}
+                    </td>
+                    <td>
+                        {item.latestRev}
+                    </td>
+                    <td>
+                        {item.prevRev}
+                    </td>
+                    <td>
+                        {item.quarterVer}
+                    </td>
+                    <td>
+                        {item.assessed}
+                    </td>
+                    <td>
+                        {item.submitted}
+                    </td>
+                    <td>
+                        {item.accepted}
+                    </td>
+                    <td>
+                        {item.rejected}
+                    </td> <td>
+                        {item.cat3}
+                    </td>
+                    <td>
+                        {item.cat2}
+                    </td>
+                    <td>
+                        {item.cat1}
+                    </td>
+                </tr>
+            );
+        case '9':
+            return (
+                <tr key={index}>
+                    <td>
+                        {index}
+                    </td>
+                    <td>
+                        {item.emass}
+                    </td>
+                    <td>
+                        {item.collectionName}
+                    </td>
+                    <td>
+                        {item.benchmark}
+                    </td>
+                    <td>
+                        {item.latestRev}
+                    </td>
+                    <td>
+                        {item.prevRev}
+                    </td>
+                    <td>
+                        {item.quarterVer}
+                    </td>
+                    <td>
+                        {item.groupId}
+                    </td>
+                    <td>
+                        {item.asset}
+                    </td>
+                    <td>
+                        {item.Result}
+                    </td>
+                    <td>
+                        {item.detail}
+                    </td>
+                    <td>
+                        {item.comment}
+                    </td>
+                    <td>
+                        {item.status}
+                    </td>
+                </tr>
+            );
+        default:
+            return null;
+    }
+}
